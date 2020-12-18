326857ea5dc778b3959dd705b836feec9d076b75
Saori Costa <saoricosta@gmail.com>
2020-12-18 19:33:34 -0300

    adicao de cors no controller

--- src/main/java/com/teammacc/delivery/controller/DeliveryController.java
+++ src/main/java/com/teammacc/delivery/controller/DeliveryController.java
@@ -2,6 +2,7 @@ package com.teammacc.delivery.controller;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.teammacc.delivery.data.vo.DeliveryVO;
 import com.teammacc.delivery.service.DeliveryService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/delivery")
 public class DeliveryController {
